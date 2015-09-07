92acf5e941b2f61cc7b06e910a2fc9473ce1688d
Guoleming <1242575323@qq.com>
2015-09-07 15:26:32 +0800

    作业提交

---
+++ src/com/qilu/test/SimpleThread.java
@@ -0,0 +1,31 @@
+package com.qilu.test;
+
+import org.junit.Test;
+
+public class SimpleThread extends Thread {
+	private int countDown = 5;
+	private int threadNumber;
+	private static int threadCount = 0;
+
+	public SimpleThread() {
+		threadNumber = ++threadCount;
+		System.out.println("Making " + threadNumber);
+	}
+  @Test
+	public void run() {
+		while (true) {
+			System.out
+					.println("Thread " + threadNumber + "(" + countDown + ")");
+			if (--countDown == 0)
+				return;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 5; i++)
+			new SimpleThread().start();
+		System.out.println("All Threads Started");
+	}
+        
+	
+} 
